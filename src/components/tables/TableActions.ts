7b92803aee8e2df69a765572d006a97601ad0c00
Pierre-Olivier Chretien <pochretien@coveo.com>
2018-05-07 15:26:34 -0400

    format

--- src/components/tables/TableActions.ts
+++ src/components/tables/TableActions.ts
@@ -1,66 +1,66 @@
 <<<<<<< HEAD
-import { IReduxAction } from '../../utils/ReduxUtils';
-import { ITablePredicate } from './Table';
-import { ITableData, ITableState } from './TableReducers';
+import {IReduxAction} from '../../utils/ReduxUtils';
+import {ITablePredicate} from './Table';
+import {ITableData, ITableState} from './TableReducers';
 
 export type ITableStateModifier = (state: ITableState) => ITableState;
 
 export const TableActions = {
-  add: 'ADD_TABLE',
-  remove: 'REMOVE_TABLE',
-  inError: 'IN_ERROR_TABLE',
-  modifyState: 'MODIFY_STATE_TABLE',
-  updateSelectedIds: 'UPDATE_SELECTED_IDS_TABLE',
+    add: 'ADD_TABLE',
+    remove: 'REMOVE_TABLE',
+    inError: 'IN_ERROR_TABLE',
+    modifyState: 'MODIFY_STATE_TABLE',
+    updateSelectedIds: 'UPDATE_SELECTED_IDS_TABLE',
 };
 
 export interface ITableActionPayload {
-  id: string;
-  isInError?: boolean;
-  initialTableData?: ITableData;
-  initialPerPage?: number;
-  headingAttributeIds?: string[];
-  predicates?: ITablePredicate[];
-  tableStateModifier?: ITableStateModifier;
-  shouldResetPage?: boolean;
-  selectedIds?: string[];
-  hasMultipleSelectedRow?: boolean;
+    id: string;
+    isInError?: boolean;
+    initialTableData?: ITableData;
+    initialPerPage?: number;
+    headingAttributeIds?: string[];
+    predicates?: ITablePredicate[];
+    tableStateModifier?: ITableStateModifier;
+    shouldResetPage?: boolean;
+    selectedIds?: string[];
+    hasMultipleSelectedRow?: boolean;
 }
 
 export const addTable = (id: string, initialTableData: ITableData, predicates: ITablePredicate[]): IReduxAction<ITableActionPayload> => ({
-  type: TableActions.add,
-  payload: { id, initialTableData, predicates: predicates || [] },
+    type: TableActions.add,
+    payload: {id, initialTableData, predicates: predicates || []},
 });
 
 export const removeTable = (id: string): IReduxAction<ITableActionPayload> => ({
-  type: TableActions.remove,
-  payload: { id },
+    type: TableActions.remove,
+    payload: {id},
 });
 
 export const setIsInError = (id: string, isInError: boolean): IReduxAction<ITableActionPayload> => ({
-  type: TableActions.inError,
-  payload: { id, isInError },
+    type: TableActions.inError,
+    payload: {id, isInError},
 });
 
 export const modifyState = (
-  id: string,
-  tableStateModifier: ITableStateModifier,
-  shouldResetPage: boolean,
+    id: string,
+    tableStateModifier: ITableStateModifier,
+    shouldResetPage: boolean,
 ): IReduxAction<ITableActionPayload> => ({
-  type: TableActions.modifyState,
-  payload: {
-    id,
-    tableStateModifier,
-    shouldResetPage,
-  },
+    type: TableActions.modifyState,
+    payload: {
+        id,
+        tableStateModifier,
+        shouldResetPage,
+    },
 });
 
 export const updateSelectedRows = (id: string, selectedIds: string[], hasMultipleSelectedRow: boolean = false): IReduxAction<ITableActionPayload> => ({
-  type: TableActions.updateSelectedIds,
-  payload: {
-    id,
-    selectedIds,
-    hasMultipleSelectedRow,
-  },
+    type: TableActions.updateSelectedIds,
+    payload: {
+        id,
+        selectedIds,
+        hasMultipleSelectedRow,
+    },
 });
 =======
 import {IReduxAction} from '../../utils/ReduxUtils';
--- src/components/tables/TableConnected.tsx
+++ src/components/tables/TableConnected.tsx
@@ -1,63 +1,63 @@
 <<<<<<< HEAD
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
+import {connect} from 'react-redux';
+import {createSelector} from 'reselect';
 import * as _ from 'underscore';
-import { contains } from 'underscore.string';
-import { IReactVaporState } from '../../ReactVapor';
-import { IDispatch } from '../../utils/ReduxUtils';
-import { ReduxUtils } from '../../utils/ReduxUtils';
-import { IActionOptions } from '../actions/Action';
-import { addActionsToActionBar } from '../actions/ActionBarActions';
-import { IDatePickerState } from '../datePicker/DatePickerReducers';
-import { IDropdownOption } from '../dropdownSearch/DropdownSearch';
-import { closeDropdownSearch, selectOptionDropdownSearch } from '../dropdownSearch/DropdownSearchActions';
-import { IDropdownSearchState } from '../dropdownSearch/DropdownSearchReducers';
-import { IFilterState } from '../filterBox/FilterBoxReducers';
-import { IPaginationState } from '../navigation/pagination/NavigationPaginationReducers';
-import { IPerPageState } from '../navigation/perPage/NavigationPerPageReducers';
-import { IData, ITableCompositeStateProps, ITableDispatchProps, ITableOwnProps, ITableProps, Table } from './Table';
-import { addTable, removeTable } from './TableActions';
-import { TABLE_PREDICATE_DEFAULT_VALUE, TableChildComponent } from './TableConstants';
-import { defaultTableStateModifierThunk } from './TableDataModifier';
-import { ITableHeaderCellState } from './TableHeaderCellReducers';
-import { ITableById, ITableCompositeState, ITableData, ITableState } from './TableReducers';
-import { getTableChildComponentId } from './TableUtils';
+import {contains} from 'underscore.string';
+import {IReactVaporState} from '../../ReactVapor';
+import {IDispatch} from '../../utils/ReduxUtils';
+import {ReduxUtils} from '../../utils/ReduxUtils';
+import {IActionOptions} from '../actions/Action';
+import {addActionsToActionBar} from '../actions/ActionBarActions';
+import {IDatePickerState} from '../datePicker/DatePickerReducers';
+import {IDropdownOption} from '../dropdownSearch/DropdownSearch';
+import {closeDropdownSearch, selectOptionDropdownSearch} from '../dropdownSearch/DropdownSearchActions';
+import {IDropdownSearchState} from '../dropdownSearch/DropdownSearchReducers';
+import {IFilterState} from '../filterBox/FilterBoxReducers';
+import {IPaginationState} from '../navigation/pagination/NavigationPaginationReducers';
+import {IPerPageState} from '../navigation/perPage/NavigationPerPageReducers';
+import {IData, ITableCompositeStateProps, ITableDispatchProps, ITableOwnProps, ITableProps, Table} from './Table';
+import {addTable, removeTable} from './TableActions';
+import {TABLE_PREDICATE_DEFAULT_VALUE, TableChildComponent} from './TableConstants';
+import {defaultTableStateModifierThunk} from './TableDataModifier';
+import {ITableHeaderCellState} from './TableHeaderCellReducers';
+import {ITableById, ITableCompositeState, ITableData, ITableState} from './TableReducers';
+import {getTableChildComponentId} from './TableUtils';
 
 export const getTableCompositeState = (state: IReactVaporState, id: string): ITableCompositeState => {
-  const tableState: ITableState = state.tables[id] || {} as ITableState;
-  const filterState: IFilterState = tableState && _.findWhere(state.filters, { id: tableState.filterId });
-  const paginationState: IPaginationState = tableState && _.findWhere(state.paginationComposite, { id: tableState.paginationId });
-  const perPageState: IPerPageState = tableState && _.findWhere(state.perPageComposite, { id: tableState.perPageId });
-  const tableHeaderCellState: ITableHeaderCellState = tableState && state.tableHeaderCells[tableState.tableHeaderCellId];
-  const predicateStates: IDropdownSearchState[] = tableState
-    && _.reject(state.dropdownSearch, (dropdownSearch: IDropdownSearchState) => !contains(dropdownSearch.id, id)) || [];
-  const datePickerState: IDatePickerState = tableState && _.findWhere(state.datePickers, { id: tableState.datePickerRangeId });
+    const tableState: ITableState = state.tables[id] || {} as ITableState;
+    const filterState: IFilterState = tableState && _.findWhere(state.filters, {id: tableState.filterId});
+    const paginationState: IPaginationState = tableState && _.findWhere(state.paginationComposite, {id: tableState.paginationId});
+    const perPageState: IPerPageState = tableState && _.findWhere(state.perPageComposite, {id: tableState.perPageId});
+    const tableHeaderCellState: ITableHeaderCellState = tableState && state.tableHeaderCells[tableState.tableHeaderCellId];
+    const predicateStates: IDropdownSearchState[] = tableState
+        && _.reject(state.dropdownSearch, (dropdownSearch: IDropdownSearchState) => !contains(dropdownSearch.id, id)) || [];
+    const datePickerState: IDatePickerState = tableState && _.findWhere(state.datePickers, {id: tableState.datePickerRangeId});
 
-  return {
-    id: tableState.id,
-    data: tableState.data,
-    isInError: tableState.isInError,
-    isLoading: tableState.isLoading,
-    filter: filterState && filterState.filterText,
-    page: paginationState && paginationState.pageNb,
-    perPage: perPageState && perPageState.perPage,
-    sortState: {
-      attribute: tableHeaderCellState && tableHeaderCellState.attributeToSort,
-      order: tableHeaderCellState && tableHeaderCellState.sorted,
-    },
-    predicates: predicateStates.reduce((currentPredicates, nextPredicate: IDropdownSearchState) => {
-      // the attribute name is stored in the id of the dropdownSearch
-      const attributeName = nextPredicate.id.split(getTableChildComponentId(id, TableChildComponent.PREDICATE))[1];
-      const selectedOption = _.findWhere(nextPredicate.options, { selected: true });
-      return {
-        ...currentPredicates,
-        [attributeName]: selectedOption && selectedOption.value || TABLE_PREDICATE_DEFAULT_VALUE,
-      };
-    }, {}),
-    from: datePickerState && datePickerState.appliedLowerLimit,
-    to: datePickerState && datePickerState.appliedUpperLimit,
-  } as ITableCompositeState;
+    return {
+        id: tableState.id,
+        data: tableState.data,
+        isInError: tableState.isInError,
+        isLoading: tableState.isLoading,
+        filter: filterState && filterState.filterText,
+        page: paginationState && paginationState.pageNb,
+        perPage: perPageState && perPageState.perPage,
+        sortState: {
+            attribute: tableHeaderCellState && tableHeaderCellState.attributeToSort,
+            order: tableHeaderCellState && tableHeaderCellState.sorted,
+        },
+        predicates: predicateStates.reduce((currentPredicates, nextPredicate: IDropdownSearchState) => {
+            // the attribute name is stored in the id of the dropdownSearch
+            const attributeName = nextPredicate.id.split(getTableChildComponentId(id, TableChildComponent.PREDICATE))[1];
+            const selectedOption = _.findWhere(nextPredicate.options, {selected: true});
+            return {
+                ...currentPredicates,
+                [attributeName]: selectedOption && selectedOption.value || TABLE_PREDICATE_DEFAULT_VALUE,
+            };
+        }, {}),
+        from: datePickerState && datePickerState.appliedLowerLimit,
+        to: datePickerState && datePickerState.appliedUpperLimit,
+    } as ITableCompositeState;
 };
 
 const getDataById = (data: ITableData): ITableById => data.byId;
@@ -69,80 +69,80 @@ const getActions = (data: ITableData, props: ITableOwnProps): (rowData?: IData) 
 const getMultiSelect = (data: ITableData, props: ITableOwnProps): boolean => props.rowsMultiSelect;
 
 const actionsSelector = createSelector(
-  [
-    getDataById,
-    getSelectedIds,
-    getMultiSelect,
-    getActions,
-  ],
-  (byId: ITableById, selectedIds: string[], isMultiSelect: boolean, getActionsProp: (rowData?: IData) => IActionOptions[]): IActionOptions[] => {
-    const rowsData = selectedIds.map((id: string) => byId[id]).filter(Boolean);
-    if (getActionsProp && rowsData.length) {
-      const actions: IActionOptions[] = getActionsProp(rowsData[0]);
-      return isMultiSelect && selectedIds.length >= 2
-        ? _.filter(actions, (action: IActionOptions) => !!action.grouped)
-        : actions;
-    }
-    return [];
-  },
+    [
+        getDataById,
+        getSelectedIds,
+        getMultiSelect,
+        getActions,
+    ],
+    (byId: ITableById, selectedIds: string[], isMultiSelect: boolean, getActionsProp: (rowData?: IData) => IActionOptions[]): IActionOptions[] => {
+        const rowsData = selectedIds.map((id: string) => byId[id]).filter(Boolean);
+        if (getActionsProp && rowsData.length) {
+            const actions: IActionOptions[] = getActionsProp(rowsData[0]);
+            return isMultiSelect && selectedIds.length >= 2
+                ? _.filter(actions, (action: IActionOptions) => !!action.grouped)
+                : actions;
+        }
+        return [];
+    },
 );
 
 const mapStateToProps = (state: IReactVaporState, ownProps: ITableOwnProps): ITableCompositeStateProps => {
-  const table: ITableState = state.tables && state.tables[ownProps.id];
-  return {
-    tableCompositeState: getTableCompositeState(state, ownProps.id),
-    actions: table && table.data ? actionsSelector(table.data, ownProps) : [],
-  };
+    const table: ITableState = state.tables && state.tables[ownProps.id];
+    return {
+        tableCompositeState: getTableCompositeState(state, ownProps.id),
+        actions: table && table.data ? actionsSelector(table.data, ownProps) : [],
+    };
 };
 
 const mapDispatchToProps = (
-  dispatch: IDispatch,
-  ownProps: ITableOwnProps,
+    dispatch: IDispatch,
+    ownProps: ITableOwnProps,
 ): ITableDispatchProps => ({
-  onDidMount: () => {
-    dispatch(
-      addTable(
-        ownProps.id,
-        ownProps.initialTableData,
-        ownProps.predicates,
-      ),
-    );
-  },
-  onUnmount: () => {
-    dispatch(removeTable(ownProps.id));
-  },
-  onWillUpdate: (actions: IActionOptions[]) => {
-    if (actions.length) {
-      dispatch(addActionsToActionBar(getTableChildComponentId(ownProps.id, TableChildComponent.ACTION_BAR), actions));
-    }
-  },
-  onModifyData: (
-    shouldResetPage: boolean,
-    tableCompositeState: ITableCompositeState,
-    previousTableCompositeState: ITableCompositeState,
-  ) => {
-    if (ownProps.manual) {
-      dispatch(ownProps.manual(ownProps, shouldResetPage, tableCompositeState, previousTableCompositeState));
-    } else {
-      dispatch(defaultTableStateModifierThunk(ownProps, shouldResetPage, tableCompositeState));
-    }
-  },
-  onRowClick: (actions: IActionOptions[] = [], numberOfSelectedIds: number) => {
-    dispatch(
-      addActionsToActionBar(
-        getTableChildComponentId(ownProps.id, TableChildComponent.ACTION_BAR),
-        ownProps.rowsMultiSelect && numberOfSelectedIds >= 2 ? _.filter(actions, (action: IActionOptions) => !!action.grouped) : actions,
-      ),
-    );
-  },
-  onPredicateOptionClick: (predicateId: string, option: IDropdownOption) => {
-    dispatch(selectOptionDropdownSearch(predicateId, option));
-    dispatch(closeDropdownSearch(predicateId));
-  },
+    onDidMount: () => {
+        dispatch(
+            addTable(
+                ownProps.id,
+                ownProps.initialTableData,
+                ownProps.predicates,
+            ),
+        );
+    },
+    onUnmount: () => {
+        dispatch(removeTable(ownProps.id));
+    },
+    onWillUpdate: (actions: IActionOptions[]) => {
+        if (actions.length) {
+            dispatch(addActionsToActionBar(getTableChildComponentId(ownProps.id, TableChildComponent.ACTION_BAR), actions));
+        }
+    },
+    onModifyData: (
+        shouldResetPage: boolean,
+        tableCompositeState: ITableCompositeState,
+        previousTableCompositeState: ITableCompositeState,
+    ) => {
+        if (ownProps.manual) {
+            dispatch(ownProps.manual(ownProps, shouldResetPage, tableCompositeState, previousTableCompositeState));
+        } else {
+            dispatch(defaultTableStateModifierThunk(ownProps, shouldResetPage, tableCompositeState));
+        }
+    },
+    onRowClick: (actions: IActionOptions[] = [], numberOfSelectedIds: number) => {
+        dispatch(
+            addActionsToActionBar(
+                getTableChildComponentId(ownProps.id, TableChildComponent.ACTION_BAR),
+                ownProps.rowsMultiSelect && numberOfSelectedIds >= 2 ? _.filter(actions, (action: IActionOptions) => !!action.grouped) : actions,
+            ),
+        );
+    },
+    onPredicateOptionClick: (predicateId: string, option: IDropdownOption) => {
+        dispatch(selectOptionDropdownSearch(predicateId, option));
+        dispatch(closeDropdownSearch(predicateId));
+    },
 });
 
 export const TableConnected: React.ComponentClass<ITableProps> =
-  connect(mapStateToProps, mapDispatchToProps, ReduxUtils.mergeProps)(Table);
+    connect(mapStateToProps, mapDispatchToProps, ReduxUtils.mergeProps)(Table);
 =======
 import * as React from 'react';
 import {connect} from 'react-redux';
